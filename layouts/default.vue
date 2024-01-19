e9db110d32254cc0f4cd2ced4750418a312b47cd
kian-404 <tianjianen2015@gmail.com>
2024-01-19 16:07:22 +0800

    🎈 perf: update layout

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <UContainer class="layout">
+    <Header />
+    <div class="content">
+      <Sidebar  v-if="false"/>
+      <div class="main">
+        <slot />
+      </div>
+    </div>
+  </UContainer>
+</template>
+
+<script setup>
+import Header from "./header.vue";
+import Sidebar from "./sidebar.vue";
+</script>
+
+<style lang="less" scoped>
+.layout {
+  height: 100vh;
+  padding: 0;
+  .content {
+    height: calc(100% - 48px);
+    display: flex;
+    .main {
+      flex: 1;
+    }
+  }
+}
+</style>
---
+++ layouts/header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="header">
+    <UIcon v-show="colorMode.preference === 'light'" class="icon-item" name="i-heroicons-moon"  @click="changeTheme('dark')"/>
+    <UIcon v-show="colorMode.preference === 'dark'" class="icon-item" name="i-heroicons-sun"   @click="changeTheme('light')"/>
+  </div>
+</template>
+
+<script setup>
+const colorMode = useColorMode()
+
+console.log(colorMode.preference)
+const changeTheme = (colorTheme) => {
+  console.log(colorTheme)
+  console.log(colorMode)
+
+  colorMode.preference = colorTheme
+}
+</script>
+
+<style lang="less" scoped>
+.header {
+    margin: 0px 20px;
+    display: flex;
+    align-items: center;
+    height: 48px;
+    justify-content: right;
+  .icon-item{
+    width: 1.8rem;
+    height: 1.8rem;
+  }
+}
+body {
+  background-color: #fff;
+  color: rgba(0,0,0,0.8);
+}
+.dark-mode body {
+  background-color: #091a28;
+  color: #ebf4f1;
+}
+.sepia-mode body {
+  background-color: #f1e7d0;
+  color: #433422;
+}
+</style>

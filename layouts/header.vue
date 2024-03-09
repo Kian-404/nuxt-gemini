8665324efb677eb3c177ba410eb4dfc96016e8c6
kian-404 <tianjianen2015@gmail.com>
2024-03-09 18:12:19 +0800

    🦄 refactor: add project title

--- layouts/header.vue
+++ layouts/header.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="header">
+    <div class="title">Y Gemini</div>
     <UIcon v-show="colorTheme.theme !== 'dark'" class="icon-item" name="i-heroicons-moon"  @click="colorTheme.changeTheme('dark')"/>
     <UIcon v-show="colorTheme.theme === 'dark'" class="icon-item" name="i-heroicons-sun"   @click="colorTheme.changeTheme('light')"/>
   </div>
@@ -15,7 +16,13 @@ const colorTheme = useThemeStore();
     display: flex;
     align-items: center;
     height: 48px;
-    justify-content: right;
+    .title {
+      font-family: "Josefin Sans", sans-serif;
+      font-size: 24px;
+      font-weight: 700;
+      margin-right: auto;
+      flex: 1;
+    }
   .icon-item{
     width: 1.8rem;
     height: 1.8rem;

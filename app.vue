405d84248a994aa98b6050a39c9f90a243acb2df
kian-404 <tianjianen2015@gmail.com>
2024-03-10 15:40:11 +0800

    🌈 style: add title

--- app.vue
+++ app.vue
@@ -3,3 +3,16 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+<script setup lang="ts">
+useHead({
+  title: 'Y Gemini',
+  meta: [
+    { name: 'description', content: 'YourSelf Gemini' },
+    { name: 'keywords', content: 'gemini, yourself' },
+    {name: 'viewport', content: 'width=device-width, initial-scale=1, , maximum-scale=1.0, user-scalable=0'}
+  ],
+
+  link: [ { rel: 'icon', type: 'image/x-icon', href: '/svg/gemini.svg' } ]
+})
+
+</script>
